27e28345ea0401b60406ba9e9a60efd3f59611d5
omgKEtCHUP <theamazinketchup@gmail.com>
2014-03-03 11:06:45 -0500

    Order by percentage in Analytics

--- public/javascripts/inquizitor.js
+++ public/javascripts/inquizitor.js
@@ -488,4 +488,21 @@ var app = angular.module('app', [ 'ngAnimate','ui.router', 'ui.bootstrap', 'ngSa
 	        });
 	    }
 	}])
+	.filter('orderObjectBy', function(){
+	 	return function(input, attribute) {
+		    if (!angular.isObject(input)) return input;
+
+		    var array = [];
+		    for(var objectKey in input) {
+		        array.push(input[objectKey]);
+		    }
+
+		    array.sort(function(a, b){
+		        a = parseInt(a[attribute]);
+		        b = parseInt(b[attribute]);
+		        return a - b;
+		    });
+		    return array;
+		}
+	});
 ;
