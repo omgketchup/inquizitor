24c46b6bff4365143ee00e4410901c52c873c49d
omgKEtCHUP <theamazinketchup@gmail.com>
2014-03-03 11:54:37 -0500

    Sorting fix

--- public/javascripts/inquizitor.js
+++ public/javascripts/inquizitor.js
@@ -498,10 +498,10 @@ var app = angular.module('app', [ 'ngAnimate','ui.router', 'ui.bootstrap', 'ngSa
 		    }
 
 		    array.sort(function(a, b){
-		        a = parseInt(a[attribute]);
-		        b = parseInt(b[attribute]);
-		        return a - b;
-		    });
+				var alc = a[attribute].toLowerCase(),
+				    blc = b[attribute].toLowerCase();
+				return alc > blc ? 1 : alc < blc ? -1 : 0;
+			});
 		    return array;
 		}
 	});
