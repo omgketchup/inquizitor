ce2a6cf0fc747465efb8b2650c0405b87f04d871
omgKEtCHUP <theamazinketchup@gmail.com>
2014-04-18 11:03:24 -0400

    Debugging on prod is always good

--- public/javascripts/inquizitor.js
+++ public/javascripts/inquizitor.js
@@ -81,8 +81,9 @@ var app = angular.module('app', [ 'ngAnimate','ui.router', 'ui.bootstrap', 'ngSa
 				for(var i = 0; i<data.length; i++){
 					var row = data[i];
 					var answer = row.answers[1].response;
+					console.log("ANSWER: " + answer);
 					var betteranswer = answer.replace(/\,/g,"&comma; ");
-					var rowstring = "\"" + row.email + "\"" + "¿" + "\"" + answer + "\"" + ";\r\n";
+					var rowstring = "\"" + row.email + "\"" + "¿" + "\"" + betteranswer + "\"" + ";\r\n";
 					csvString += rowstring;
 				}
 				console.log(csvString);
